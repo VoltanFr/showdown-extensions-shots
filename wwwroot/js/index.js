fef82e23a0ad2a46a116ff196dac9124b3df473e
Vincent <mahonv@gmail.com>
2022-04-02 09:26:57 +0200

    Introduce showdown

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,8 +1,17 @@
-﻿function buttonClicked() {
+﻿function convertMarkdown(src) {
+    var converter = new showdown.Converter({ tables: true });
+    converter.setOption('openLinksInNewWindow', 'true');
+    converter.setOption('simplifiedAutoLink', 'true');
+    converter.setOption('simpleLineBreaks', 'true');
+    converter.setOption('noHeaderId', 'true');
+    return converter.makeHtml(src);
+}
+
+function buttonClicked() {
     const inputTextArea = document.getElementById('inputTextArea');
     const inputText = inputTextArea.value;
 
-    var output = "<p><strong>" + inputText + "</strong></p>";
+    var output = convertMarkdown(inputText);
 
     const renderedDiv = document.getElementById('renderedDiv');
     renderedDiv.innerHTML = output;
