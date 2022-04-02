d36dc53c8ac826fabe7be0f4d577fa0cae1e9b59
Vincent <mahonv@gmail.com>
2022-04-02 09:18:24 +0200

    Initial commit (showdown not introduced yet)

---
+++ wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿function buttonClicked() {
+    const inputTextArea = document.getElementById('inputTextArea');
+    const inputText = inputTextArea.value;
+
+    var output = "<p><strong>" + inputText + "</strong></p>";
+
+    const renderedDiv = document.getElementById('renderedDiv');
+    renderedDiv.innerHTML = output;
+}
